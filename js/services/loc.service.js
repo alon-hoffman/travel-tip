93e5b90d90f1f184ea754a8662c9601b4d7978b7
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-11-02 14:26:08 +0200

    Added create and update loc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,21 @@
+import utils from './utils.js'
+import { storage } from './storage.service.js'
+
 export const locService = {
-    getLocs
+    getLocs,
+    createLoc,
+    initCache
+}
+
+var locCache = []
+
+function initCache() {
+    if (storage.load('locations')) locCache = storage.load('locations')
 }
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -15,5 +26,33 @@ function getLocs() {
         }, 2000)
     })
 }
+
+//CRUD*********************************8
+
+function createLoc(loc, name) {
+    const newLoc = {
+        id: utils.makeId,
+        name,
+        loc,
+    }
+    locCache.unshift(newLoc)
+    storage.save('locations', locCache)
+}
+
+function updateLocName(id, newName) {
+    const loc = getLoc(id)
+    loc.name = newName
+}
+
+
+
+// Inner utils***************************
+
+function getLoc(id) {
+    return locCache.find(location => id == location.id)
+}
+window.createLoc = createLoc
+
+
 
 
